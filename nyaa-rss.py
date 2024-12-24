dc61b99a6301ba1de4017616b1893ca41c4ea3ae
leivysallia <aelenydd@gmail.com>
2024-12-24 01:42:31 -0500

    redux

--- nyaa-rss.py
+++ nyaa-rss.py
@@ -1,73 +1,93 @@
-import datetime
+import atexit, datetime, os, re, requests, sys
 from datetime import timedelta
-from urllib.request import urlopen
-import re, os
+from os.path import expanduser
 
+def debug():
+    atexit.register(lambda: input("Press Enter To Quit."))
 def clear_console():
     if os.name == 'nt':
         _ = os.system('cls')
     else:
         _ = os.system('clear')
-def rss(bob):
-    url = (bob)
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    hashpattern = "<.*Hash.*Hash>"
-    match_results = re.findall(hashpattern, html, re.IGNORECASE)
-    for match in match_results:
-        link = re.sub("^.*?<.*?>", "", match)
-        link = re.sub("<.*>$", '', link)
-        if (link not in library):
-            hashset.add(link)
-        else:
-            print(link + ' is already accounted for...')
-    linkpattern = "<.*http.*torrent.*>"
-    match_results = re.findall(linkpattern, html, re.IGNORECASE)
-    for match in match_results:
-        link = re.sub("^.*?<.*?>", "", match)
-        link = re.sub("<.*>$", '', link)
-        if (link not in library):
-            linkset.add(link)
-        else:
-            print(link + ' is already accounted for...')
-    yield hashset
-    yield linkset
-def checkdate():
+def file_not_found():
+    sys.exit()
+def check_files():
+    if not os.path.exists(codexlst):
+        file = open(codexlst, 'w')
+        file.close
+    if not os.path.exists(prevlst):
+        file = open(prevlst, 'w')
+        print('19700101', file=file)
+        file.close()
+    if not os.path.exists(feedslst):
+        file = open(feedslst, 'w')
+        file.close
+        print('there are no feeds specified in feeds.lst\nplease find the feeds.lst in ' + feedslst + '\nremember that the only feeds that are currently supported are ones from sukebei.nyaa.si and nyaa.si\nthank you.')
+        file_not_found()
+def read_rss(url):
+    page = requests.get(url)
+    html = page.text.splitlines()
+    for line in html:
+        parseline = str(line)
+        pattern = '.*<title.*title>'
+        if re.match(pattern, parseline):
+            parsed = re.sub("^.*?<.*?>", "", parseline)
+            parsed = re.sub("<.*>$", '', parsed)
+            parsed = re.sub('&#34;', "'", parsed)
+            templist.append(parsed)
+        pattern = '.*<link.*link>'
+        if re.match(pattern, parseline):
+            parsed = re.sub("^.*?<.*?>", "", parseline)
+            parsed = re.sub("<.*>$", '', parsed)
+            parsed = re.sub('&#34;', "'", parsed)
+            templist.append(parsed)
+        pattern = '.*<.*Hash.*Hash>'
+        if re.match(pattern, parseline):
+            parsed = re.sub("^.*?<.*?>", "", parseline)
+            parsed = re.sub("<.*>$", '', parsed)
+            parsed = re.sub('&#34;', "'", parsed)
+            templist.append(parsed)
+    return templist
+def check_date():
     curr = datetime.datetime.now()
     curr = curr.strftime('%Y%m%d')
-    prev = str(open('S:/Codex/Documents/GitHub/nyaa-rss/.calc/prev.lst').read())
-    calc = datetime.datetime.strptime(prev, '%Y%m%d')
+    prev = str(open(prevlst).read().split())
+    calc = datetime.datetime.strptime(prev, "['%Y%m%d']")
     calc = calc + timedelta(days = 5)
     calc = calc.strftime('%Y%m%d')
-    calc = int(calc)
-    prev = int(prev)
-    curr = int(curr)
-    if curr >= calc: 
-        log = open('S:/Codex/Documents/GitHub/nyaa-rss/.calc/prev.lst', 'w')
+    if curr >= calc:
+        log = open(prevlst, 'w')
         print(curr, file=log)
-        return 'one'
+        return 'run'
     else:
-        return 'none'
+        force = input('it is not time to run program.\nwould you like to force update anyway?\ntype run to force update: ')
+        return force
 
+home = expanduser('~')
+data = '/.config/nyaa-rss/'
+codexlst = str(home + data + 'codex.lst').replace('\\', '/')
+feedslst = str(home + data + 'feeds.lst').replace('\\', '/')
+prevlst = str(home + data + 'prev.lst').replace('\\', '/')
+
+debug()
 clear_console()
-isdo = checkdate()
+check_files()
+run = check_date()
+templist = []
 
-if isdo == 'one':
-    hashset = set()
-    linkset = set()
-    library = set(open('S:/Codex/Documents/GitHub/nyaa-rss/codex.lst').read().split())
-    feeds = set(open('S:/Codex/Documents/GitHub/nyaa-rss/feeds.lst').read().split()) # Open file on read mode
-    codexlst = open("S:/Codex/Documents/GitHub/nyaa-rss/codex.lst", "a")
-
-    for bob in feeds:
-        result = rss(bob)
-        for hashs in next(result):
-            print(hashs)
-            print(hashs, file=codexlst)
-        for links in next(result):
-            print(links)
-            print(links, file=codexlst)
-    input('all feeds processed...\npress enter to quit...')
+if run == 'run':
+    library = set(open(codexlst).read().split())
+    feeds = set(open(feedslst).read().split())
+    codex = open(codexlst, "a", encoding="utf-8")
+    pattern = r'Nyaa.*Torrent File RSS'
+    for uri in feeds:
+        output = read_rss(uri)
+        for line in output:
+            if line not in library:
+                if line != 'https://nyaa.si/':
+                    if not re.match(pattern, line):
+                        print(line, file=codex)
+                        print(line)
+    input('all feeds processed.\npress enter to quit.')
 else:
-    input('no feeds updated...\nto reduce trafic, try again later...\npress enter to quit...')
-
+    input('|:NO FEEDS UPDATED:|\npress enter to quit.')
